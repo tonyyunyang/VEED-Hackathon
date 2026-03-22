5867b501f68af63a80be4b80ab10c6c8183a9b76
tony <tonyyunyang@outlook.com>
2026-03-22 03:38:35 +0100

    chore: align local setup defaults

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    '.conda-envs/**',
+    '.conda-pkgs/**',
+    '.uv-cache/**',
+    'server/.venv/**',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
